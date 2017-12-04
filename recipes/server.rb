7bbd9e18bf2a2e723ed74fb0b361de2488f8d5b4
Michael Segulja <michael@segulja.com>
2017-12-04 10:45:42 -0600

    Changes:
      - Add forwarders to 9.9.9.9 and 8.8.8.8
      - Add notifies action to restart service named after changes to any
        templates
      - Bumped version to 1.1.0
    
    	modified:   metadata.rb
    	modified:   recipes/server.rb
    	modified:   templates/db.172.16.20.erb
    	modified:   templates/db.segulja.com.erb
    	modified:   templates/named.conf.erb

--- recipes/server.rb
+++ recipes/server.rb
@@ -13,34 +13,38 @@ packages.each do |pkg|
 end
 
 directory '/etc/named/zones' do
-  owner   'root'
-  group   'named'
-  mode    '0770'
-  action  :create
+  owner    'root'
+  group    'named'
+  mode     '0770'
+  action   :create
+  notifies :restart, 'service[named]', :immediately
 end
 
 template '/etc/named/zones/db.segulja.com' do
-  source  'db.segulja.com.erb'
-  owner   'root'
-  group   'named'
-  mode    '0640'
-  action  :create
+  source   'db.segulja.com.erb'
+  owner    'root'
+  group    'named'
+  mode     '0640'
+  action   :create
+  notifies :restart, 'service[named]', :immediately
 end
 
 template '/etc/named/zones/db.172.16.20' do
-  source  'db.172.16.20.erb'
-  owner   'root'
-  group   'named'
-  mode    '0640'
-  action  :create
+  source   'db.172.16.20.erb'
+  owner    'root'
+  group    'named'
+  mode     '0640'
+  action   :create
+  notifies :restart, 'service[named]', :immediately
 end
 
 template '/etc/named.conf' do
-  source  'named.conf.erb'
-  owner   'root'
-  group   'named'
-  mode    '0640'
-  action  :create
+  source   'named.conf.erb'
+  owner    'root'
+  group    'named'
+  mode     '0640'
+  action   :create
+  notifies :restart, 'service[named]', :immediately
 end
 
 service 'named' do
