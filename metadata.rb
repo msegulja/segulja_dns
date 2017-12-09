c23e96c1909d1dff3d78fd69065a0a3171ce0a7b
Michael Segulja <michael@segulja.com>
2017-12-09 17:25:03 -0600

    Changes:
      - Added an A record for host ovmm
      - Bumped version to 1.2.1
    
    	modified:   metadata.rb
    	modified:   templates/db.172.16.20.erb
    	modified:   templates/db.segulja.com.erb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures segulja_dns'
 long_description 'Installs/Configures segulja_dns'
-version '1.2.0'
+version '1.2.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
